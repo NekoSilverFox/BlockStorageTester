d7553446d627f9f20513b9c9c84d2cbd45867b90
冰糖雪狸 <weidufox@gmail.com>
2024-09-20 22:04:24 +0300

    将子线程数据库连接封装为单独类

---
+++ AsyncComputeModule.cpp
@@ -0,0 +1,101 @@
+#include "AsyncComputeModule.h"
+#include "ThemeStyle.h"
+
+#include <QDebug>
+#include <QThread>
+
+/**
+ * 注意：这个类中所有的方法都是准备放置在子线程中执行的，内部包含了耗时的复杂计算任务
+ */
+
+AsyncComputeModule::AsyncComputeModule(QObject *parent)
+    : QObject{parent}
+{
+    _dbs = new DatabaseService(this);
+
+    emit signalWriteSuccLog(QString("Thread %1 init successed").arg(getCurrentThreadID()));
+}
+
+/**
+ * @brief AsyncComputeModule::connectDatabase 连接到指定数据库（由于数据库操作不能跨线程，所以线程中要单独链接到数据库）
+ * @param host 主机名或链接
+ * @param port 端口号
+ * @param driver Qt 数据库驱动
+ * @param user 用户名
+ * @param pwd 密码
+ * @param database 数据库名称
+ * @return 是否成功连接
+ */
+bool AsyncComputeModule::connectDatabase(const QString &host, const int port, const QString &driver, const QString &user, const QString &pwd, const QString &database)
+{
+    emit signalWriteInfoLog(QString("Thread %1: Start connect to %2:%3 database %4").arg(getCurrentThreadID(), host, QString::number(port), database));
+
+    bool is_succ = _dbs->connectDatabase(host, port, driver, user, pwd, database);
+    _last_log = QString("Thread %1: %2").arg(getCurrentThreadID(), _dbs->lastLog());
+    if (!is_succ)
+    {
+        emit signalDbConnState(_dbs->isDatabaseOpen());
+        emit signalWriteErrorLog(_last_log);
+        emit signalErrorBox(_last_log);
+        return false;
+    }
+    emit signalDbConnState(_dbs->isDatabaseOpen());
+    emit signalWriteSuccLog(_last_log);
+    emit signalInfoBox(_last_log);
+    return true;
+}
+
+/**
+ * @brief AsyncComputeModule::disconnectCurrentDatabase 断开当前线程与当前数据库的连接
+ */
+void AsyncComputeModule::disconnectCurrentDatabase()
+{
+    bool is_succ = _dbs->disconnectCurDatabase();
+    _last_log = QString("Thread %1: %2").arg(getCurrentThreadID(), _dbs->lastLog());
+#if !QT_NO_DEBUG
+    qDebug() << _last_log;
+#endif
+    if (!is_succ)
+    {
+        emit signalWriteErrorLog(_last_log);
+        return;
+    }
+    emit signalWriteSuccLog(_last_log);
+    emit signalDbConnState(false);
+}
+
+void AsyncComputeModule::dropCurrentDatabase()
+{
+    bool is_succ = _dbs->dropCurDatabase();
+    _last_log = QString("Thread %1: %2").arg(getCurrentThreadID(), _dbs->lastLog());
+#if !QT_NO_DEBUG
+    qDebug() << _last_log;
+#endif
+    if (!is_succ)
+    {
+        emit signalWriteErrorLog(_last_log);
+        return;
+    }
+    emit signalWriteSuccLog(_last_log);
+}
+
+/**
+ * @brief AsyncComputeModule::finishCompute 结束计算任务（将会结束改线程）
+ * @param drop_db 是否删除数据库
+ */
+void AsyncComputeModule::finishCompute(const bool drop_db)
+{
+    if (drop_db)
+    {
+        dropCurrentDatabase();
+    }
+    disconnectCurrentDatabase();
+    emit signalFinished();
+    return;
+}
+
+QString AsyncComputeModule::getCurrentThreadID() const
+{
+    return QString::number(reinterpret_cast<quintptr>(QThread::currentThreadId()), 16);
+}
+
---
+++ AsyncComputeModule.h
@@ -0,0 +1,58 @@
+#ifndef ASYNCCOMPUTEMODULE_H
+#define ASYNCCOMPUTEMODULE_H
+
+#include "DatabaseService.h"
+
+#include <QObject>
+
+/**
+ * @brief [Asynchronous Computation Module] 异步计算模块，执行所需的数据库、文件IO、计算等复杂的或耗时的计算任务。注意：最好使用单独的线程调用这个模块
+ */
+class AsyncComputeModule : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AsyncComputeModule(QObject *parent = nullptr);
+
+
+    bool connectDatabase(const QString& host, const int port, const QString& driver,
+                         const QString& user, const QString& pwd, const QString& database);
+    void disconnectCurrentDatabase();
+    void dropCurrentDatabase();
+    void finishCompute(const bool drop_db);
+
+
+signals:
+    /* 设置指示灯 */
+    void signalSetLbDBConnectedStyle(QString style);
+
+    /* 日志信号 */
+    void signalWriteInfoLog(const QString& msg);
+    void signalWriteWarningLog(const QString& msg);
+    void signalWriteErrorLog(const QString& msg);
+    void signalWriteSuccLog(const QString& msg);
+
+    /* 让主线程调用 QMessageBox 信号 */
+    void signalInfoBox(const QString& msg);
+    void signalWarnBox(const QString& msg);
+    void signalErrorBox(const QString& msg);
+
+
+    /* 数据库信号 */
+    void signalFinished(const bool drop_db);  // 任务完成信号，用于通知线程退出
+    void signalConnDb(const QString& host, const int port, const QString& driver,
+                      const QString& user, const QString& pwd, const QString& database);  // 连接数据库
+    void signalDisconnDb();
+    void signalDbConnState(const bool is_conn);  // 当前连接状态
+    void signalDropCurDb();
+
+private:
+    QString getCurrentThreadID() const;
+
+private:
+    DatabaseService* _dbs; // 当前操作的数据库对象
+    QString _last_log;     // 最后一条日志信息
+
+};
+
+#endif // ASYNCCOMPUTEMODULE_H
